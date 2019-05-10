a903b7029ce21e75ba39cabcc8a3def13ce80f87
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-05-10 11:47:47 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,11 @@ class SessionsController < ApplicationController
   end
 
   def create
+    session[:name] = params[:name]
+    redirect_to '/'
   end
 
   def destroy
+    session.delete :name
   end
 end
